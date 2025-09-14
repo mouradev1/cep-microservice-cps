8dca177406631afd7e8cc934de31c677463de4d0
Moura <mouralol23@gmail.com>
2025-09-14 13:07:26 -0300

    feat: add root endpoint with status message and version info

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.status(200).send({
+        message: "API de Consulta de CEP está funcionando!",
+        version: "1.2.1",
+        status: "OK"
+    });
+});
+
 app.use("/cep", cepRoutes);
 
 export default app;
